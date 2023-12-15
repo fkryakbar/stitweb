165a898f4ba1044e060c8ccae45f69ec044b1947
fkrydev <fikri.afa289@gmail.com>
2023-12-15 16:53:15 +0800

    fix bug 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/assets/logo.png",
+                "resources/assets/main-logo.png",
+            ],
             refresh: true,
         }),
     ],
