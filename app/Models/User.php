2f47982a2cf6492069ecac4fb0d855c9e9e0cfd7
fkrydev <fikri.afa289@gmail.com>
2023-12-15 17:26:01 +0800

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,12 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
+    protected $guarded = [];
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -38,8 +33,4 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
 }
