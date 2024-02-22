9b729c9a0d4ead09254de00e07748cbf5c99d87e
fkrydev <fikri.afa289@gmail.com>
2024-02-22 09:28:05 +0800

    remove action create in comment

--- app/Filament/Resources/CommentResource/Pages/ManageComments.php
+++ app/Filament/Resources/CommentResource/Pages/ManageComments.php
@@ -13,7 +13,7 @@ class ManageComments extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
